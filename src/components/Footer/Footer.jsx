c194f53a72d5f9d03f8a50e88eb3260daa24afd2
Mahm0ud-HassaN <mh5647659@gmail.com>
2025-04-05 00:40:54 +0200

    edit nav footer about explore and routing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (< >
+  <div div className={`${styles.footer} fixed-bottom pt-3`}>
+  <div className="row">
+      <div className="col-md-4">
+          <i className='fas fa-home fs-5'></i>
+      </div>
+      <div className="col-md-4">
+        <h3>
+          <h4>our Compony</h4>
+        </h3>
+
+      </div>
+      <div className="col-md-4">
+      <h4>our support</h4>
+
+
+      </div>
+      <p 
+  className='text-light m-1'
+  
+>
+  all rights reserved @ mr-defence 2025
+</p>
+
+
+
+    </div>
+  </div>
+    
+  
+  </>
+  )
+}
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+
+export default function Layout() {
+  return (
+    <>
+    <Navbar/>
+    <div className="w-100" style={{
+        backgroundImage:" linear-gradient(to left, #1E1E1E, #101828 )"
+    }}>
+        <div className="container">
+      <Outlet/> 
+</div>
+    </div>
+
+  
+
+    <Footer/>
+    
+    </>
+  )
+}
